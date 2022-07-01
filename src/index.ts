7b49c4d370024eb311e0d77170aed31e95706e5a
Gál Péter <pepyta118@gmail.com>
2022-07-01 15:13:24 +0200

    feat: alter the original repo to be able to fit for mobile repo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
+import * as SecureStore from "expo-secure-store";
+
 /**
- * This library is used to handle operations related to authentication in the Motivapp ecosystem.
+ * This library is used to handle operations related to authentication in the Motivapp ecosystem.
  * The purpose of this library is to be able to swap the authentication logic in the mobile repository.
  */
 export default class Authentication {
+    private static SECURE_STORE_ACCESS_TOKEN_KEY = "access_token";
+    private static SECURE_STORE_REFRESH_TOKEN_KEY = "refresh_token";
+
     /**
      * Gets the access token from the local storage.
      * @returns The access token as string.
      */
     public static async getAccessToken(): Promise<string | null> {
-        return localStorage.getItem("access_token");
+        return SecureStore.getItemAsync(Authentication.SECURE_STORE_ACCESS_TOKEN_KEY);
     }
 
     /**
@@ -16,20 +21,20 @@ export default class Authentication {
      * @returns The refresh token as string.
      */
     public static async getRefreshToken(): Promise<string | null> {
-        return localStorage.getItem("refresh_token");
+        return SecureStore.getItemAsync(Authentication.SECURE_STORE_REFRESH_TOKEN_KEY);
     }
     
     /**
      * Saves the access token to the local storage.
      */
     public static async setAccessToken(token: string): Promise<void> {
-        return localStorage.setItem("access_token", token);
+        return SecureStore.setItemAsync(Authentication.SECURE_STORE_ACCESS_TOKEN_KEY, token);
     }
 
     /**
      * Saves the refresh token to the local storage.
      */
     public static async setRefreshToken(token: string): Promise<void> {
-        return localStorage.setItem("refresh_token", token);
+        return SecureStore.setItemAsync(Authentication.SECURE_STORE_REFRESH_TOKEN_KEY, token);
     }
 }

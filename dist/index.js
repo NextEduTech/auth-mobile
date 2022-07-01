a8c6daefbd735a100854010ac866709e7a2dc572
Gál Péter <pepyta118@gmail.com>
2022-07-01 15:19:21 +0200

    chore: add dist folder to git

---
+++ dist/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var SecureStore = _interopRequireWildcard(require("expo-secure-store"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+/**
+ * This library is used to handle operations related to authentication in the Motivapp ecosystem.
+ * The purpose of this library is to be able to swap the authentication logic in the mobile repository.
+ */
+class Authentication {
+  static SECURE_STORE_ACCESS_TOKEN_KEY = "access_token";
+  static SECURE_STORE_REFRESH_TOKEN_KEY = "refresh_token";
+  /**
+   * Gets the access token from the local storage.
+   * @returns The access token as string.
+   */
+
+  static async getAccessToken() {
+    return SecureStore.getItemAsync(Authentication.SECURE_STORE_ACCESS_TOKEN_KEY);
+  }
+  /**
+   * Gets the access token from the local storage.
+   * @returns The refresh token as string.
+   */
+
+
+  static async getRefreshToken() {
+    return SecureStore.getItemAsync(Authentication.SECURE_STORE_REFRESH_TOKEN_KEY);
+  }
+  /**
+   * Saves the access token to the local storage.
+   */
+
+
+  static async setAccessToken(token) {
+    return SecureStore.setItemAsync(Authentication.SECURE_STORE_ACCESS_TOKEN_KEY, token);
+  }
+  /**
+   * Saves the refresh token to the local storage.
+   */
+
+
+  static async setRefreshToken(token) {
+    return SecureStore.setItemAsync(Authentication.SECURE_STORE_REFRESH_TOKEN_KEY, token);
+  }
+
+}
+
+exports.default = Authentication;
